3139ff5565d09702b2f3e405dca0816cf7c41e1f
Omar Shamsul Haque <omarhaque7484@gmail.com>
2026-03-18 16:23:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def health():
     return {"ok": True}
+
+@app.post("/analyze")
+async def analyze(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type
+    }
